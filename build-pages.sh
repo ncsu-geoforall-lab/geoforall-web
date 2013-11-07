71d667454e4819d155143cf00476f48aef6a0286
Anna Petrasova <kratochanna@gmail.com>
2013-11-06 22:28:45 -0500

    courses added

--- build-pages.sh
+++ build-pages.sh
@@ -2,19 +2,19 @@
 
 OUTDIR=../osgeorel
 
-for FILE in index.html publications.html about.html
+for FILE in index.html publications.html about.html courses.html
 do
     cat head.html > $OUTDIR/$FILE
     cat $FILE >> $OUTDIR/$FILE
     cat foot.html >> $OUTDIR/$FILE
 done
 
-for FILE in *.css *.png *.json *.js
+for FILE in *.css *.png *.json *.js *.jpg
 do
     cp --update $FILE $OUTDIR
 done
 
-for DIR in publication_pictures
+for DIR in publication_pictures courses_pictures
 do
     cp --update --recursive $DIR $OUTDIR
 done
