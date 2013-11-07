5b9add31b3cc01432ae5b97a42ed7b821d7a51d3
Vaclav Petras <wenzeslaus@gmail.com>
2013-11-06 20:29:02 -0500

    documenting the auto building script and changing chmod

--- watch-and-build-pages.sh
+++ watch-and-build-pages.sh
@@ -1,9 +1,38 @@
 #!/bin/sh
 
+# Usage
+# =====
+# Create a new directory named ``osgeorel`` beside your repository
+# clone::
+#
+#   mdkir ../osgeorel
+#
+# Then run this script in separate terminal (will block the terminal)
+# in the repository directory::
+#
+# ./watch-and-build-pages.sh
+#
+# Every time you change something in HTML or CSS file the new version of
+# pages will be generated into ``../osgeorel`` directory. You just need
+# to open the generated web page (e.g., ``../osgeorel/index.html``) in
+# the web browser or just reload the page you work on.
+#
+# Dependencies
+# ============
+# You need to install watchdog Python package which provides
+# watchmedo command.
+#
+# See watchdog pypi web page for details and installation instructions:
 # https://pypi.python.org/pypi/watchdog
-# sudo pip install watchdog
-# libyaml-dev (+libyaml-0-2)
+#
+# On Ubuntu you can use::
+#
+#   sudo apt-get libyaml-dev
+#   sudo pip install watchdog
+#
+# If you don't have argcomplete installed, watchmedo will complain
+# that it is missing but we don't need it, so there is nothing to worry
+# about.
 
-
-watchmedo shell-command --patterns="*.html;*.css" --recursive --wait --command='sh build-pages.sh' .
+watchmedo shell-command --patterns="*.html;*.css" --recursive --wait --command='./build-pages.sh' .
 
