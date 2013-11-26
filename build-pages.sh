3f044e02ce8a9137d18de10d2b3ef4924fc08e5b
Vaclav Petras <wenzeslaus@gmail.com>
2013-11-26 00:21:57 -0500

    enable interpretation of escape characters for echo (behavior differs shell by shell)

--- build-pages.sh
+++ build-pages.sh
@@ -5,7 +5,7 @@ OUTDIR=../osgeorel
 for FILE in index.html publications.html about.html courses.html software.html projects.html contact.html
 do
     cat head.html > $OUTDIR/$FILE
-    echo "\n<!-- This is a generated file. Do not edit. -->\n" >> $OUTDIR/$FILE
+    echo -e "\n<!-- This is a generated file. Do not edit. -->\n" >> $OUTDIR/$FILE
     cat $FILE >> $OUTDIR/$FILE
     cat foot.html >> $OUTDIR/$FILE
 done
